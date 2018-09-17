5941b8125e06a20541d08336c8b5604b3f7fa76a
Joey Chung <joeychung960@gmail.com>
2018-09-17 12:03:45 -0700

    Adding in dependency files of ex01

--- d00/ex01/sortArts.c
+++ d00/ex01/sortArts.c
@@ -4,7 +4,7 @@
 
 int		comparator(const void *a, const void *b)
 {
-	return strcmp((const char *)(((s_art *)a)->name), (const char *)(((s_art *)b)->name));
+	return strcmp((const char *)(((struct s_art *)a)->name), (const char *)(((struct s_art *)b)->name));
 }
 
 void	sortArts(struct s_art **arts)
